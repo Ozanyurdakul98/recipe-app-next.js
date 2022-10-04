70a3ec1d0257b8f869b8175298e339dd68c0d07b
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-10-04 22:25:09 +0200

    finish Dessert description

--- pages/Dessert.jsx
+++ pages/Dessert.jsx
@@ -16,12 +16,40 @@ export default function Dessert() {
         />
       </Background>
       <DetailPage style={{ marginTop: "40px" }}>
-        <h1>Desert Recipe</h1>
+        <h1>Blueberry Muffin Cake</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat
-          maxime laudantium porro officiis sequi? Possimus, animi illo aut
-          dolorem sit blanditiis, repellat sequi exercitationem, soluta labore
-          vitae nobis corporis facilis.
+          <ul>
+            <li>45 min (Total time)</li>
+            <li>15 min (Prep time)</li>
+            <li>9 servings</li>
+          </ul>
+          Ingredients <br />
+          cooking spray 1 cup blueberries <br />1 1/2 cups all-purpose flour,
+          divided <br />
+          1/2 cup white sugar <br />
+          1/4 cup vegetable oil
+          <br />
+          1/2 cup milk <br />1 egg <br />1 teaspoon vanilla extract <br />3
+          teaspoons baking powder <br />
+          1/2 teaspoon salt <br />
+          1/2 cup brown sugar <br />
+          1/3 cup all-purpose flour
+          <br /> 2 teaspoons cinnamon <br />
+          1/4 cup butter
+          <br />
+          <br />
+          Instructions Preheat the oven to 400 degrees F (200 degrees C). Spray
+          an 8x8-inch baking pan with cooking spray. Toss blueberries and 1/2
+          cup of flour in a small bowl until blueberries are coated. Set aside.
+          Beat together sugar and oil with an electric mixer in a large bowl.
+          Add milk, egg, and vanilla extract. Mix remaining 1 cup flour, baking
+          powder, and salt in a small bowl. Add flour mixture to sugar mixture;
+          mix until just combined. fold in blueberry mixture. Pour batter into
+          the prepared baking pan. Mix together brown sugar, 1/3 cup flour, and
+          cinnamon in a bowl. Cut in butter until topping mixture is crumbly.
+          Sprinkle on top of the cake batter. Bake in the preheated oven until a
+          toothpick inserted in the center comes out clean, about 30 minutes.
+          Remove from oven and allow to cool before serving.
         </p>
       </DetailPage>
     </StyledContainer>
@@ -33,6 +61,11 @@ const StyledContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  ul {
+    margin-left: 40px;
+    margin-bottom: 40px;
+  }
 `;
 
 const Background = styled.div`
